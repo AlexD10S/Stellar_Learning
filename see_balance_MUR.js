48b7fb32269e97b6d703c5f076bb47519facb789
John Murphy <murphyjohn@users.noreply.github.com>
2018-08-15 14:45:04 +0100

    Verify asset_issuer when getting balance
    This code checks that the asset_issuer was the correct issuing account. In case the account holds assets with the same code issued from a different account (which could give the wrong output).

--- see_balance_MUR.js
+++ see_balance_MUR.js
@@ -4,21 +4,20 @@ const StellarSdk = require('stellar-sdk');
 StellarSdk.Network.useTestNetwork(); // StellarSdk.Network.usePublicNetwork();
 const server = new StellarSdk.Server('https://horizon-testnet.stellar.org'); // const server = new StellarSdk.Server('https://horizon.stellar.org');
 
-// Note: this solution trusts the accounts asset codes alone.
+// This solution checks for both the asset_code and the asset_issuer of the token/asset being checked (MUR).
 // For general accounts you may need to verify the issuing account id: b.asset_issuer
+var issuerPublicKey = "GDRZOGXOBRGWN7WG5MG3LI3PFJCVWB7XCAIHUEVFHFSE3TMCKWH4YJG7"; //get from database?
 const getBalance = (account, currency) => {
     let balance = 0;
     if (currency == 'XLM') {
         balance = Number.parseFloat(account.balances.find(b => b.asset_type == 'native').balance);
     } else {
-        balance = Number.parseFloat(account.balances.find(b => b.asset_code == currency).balance);
+        balance = Number.parseFloat(account.balances.find(b => b.asset_code == currency && b.asset_issuer == issuerPublicKey ).balance);
     }
     return balance;
 }
 
-// const keypair = StellarSdk.Keypair.fromSecret('SAAMZYIS6X44TEUFEU7SHL64O2AXAYWB6LAJTP2GOJILVOZ7OGWT5J5Y')
 const keypair = StellarSdk.Keypair.fromPublicKey('GCXYQK65HXITPWSTEP5Y4JUEVD2SU2XT45NU5ALIO3LXRENZA2XH3XN6')
-
 
 server.loadAccount(keypair.publicKey())
 .then(account => {
