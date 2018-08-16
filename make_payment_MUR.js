52ede601516e492c0ac7cef14964489c29136eb9
John Murphy <murphyjohn@users.noreply.github.com>
2018-08-16 10:13:57 +0100

    Added logic for setting which asset to send
    Can send 1 kind of MUR or multiple kinds of MUR to a recipient. 
    
    program also checks to verify the asset is from the legitimate issuer account.

--- make_payment_MUR.js
+++ make_payment_MUR.js
@@ -8,8 +8,8 @@ const server = new StellarSdk.Server('https://horizon-testnet.stellar.org'); // 
 // an arbitary list of trusted assets
 const ASSETS = {
     'MUR': new StellarSdk.Asset('MUR', 'GDRZOGXOBRGWN7WG5MG3LI3PFJCVWB7XCAIHUEVFHFSE3TMCKWH4YJG7'), //asset_code, issuing account
-    'USDT': new StellarSdk.Asset('USDT', 'GCQTGZQQ5G4PTM2GL7CDIFKUBIPEC52BROAQIAPW53XBRJVN6ZJVTG6V'),
-    'MOBI' : new StellarSdk.Asset('MOBI', 'GA6HCMBLTZS5VYYBCATRBRZ3BZJMAFUDKYYF6AH6MVCMGWMRDNSWJPIH'),
+    'MUR2': new StellarSdk.Asset('MUR2', 'GDRZOGXOBRGWN7WG5MG3LI3PFJCVWB7XCAIHUEVFHFSE3TMCKWH4YJG7'), //name, ISSUER ACC
+    'MUR3': new StellarSdk.Asset('MUR3', 'GDRZOGXOBRGWN7WG5MG3LI3PFJCVWB7XCAIHUEVFHFSE3TMCKWH4YJG7'), //name, ISSUER ACC
     'XLM': StellarSdk.Asset.native()
 }
 
@@ -20,8 +20,11 @@ const getAsset = (asset) => {
 }
 
 //must be "fromsecret" to sign a Tx with
-const sender = StellarSdk.Keypair.fromSecret('SAIEZD2ZIEARTCMQK433566CJ24YGPYET6MUSQMFUKIY7QLALW6GUG4G'); //get from database? distributor secret key
-var recipient = "GAFPZODJ27C5FNHHOVRO56VTGXYCZG2Z4FPAWISI4MFRZMNQ2LYFEWO3"; //recipient public address //TRUSTLINE HAS TO EXIST //In a real app this would be got from the QR code or pasted/typed in by the sender.
+const sender = StellarSdk.Keypair.fromSecret('S...'); //MUR Buyer 3 //get from database? distributor secret key
+var recipient = "GAFPZODJ27C5FNHHOVRO56VTGXYCZG2Z4FPAWISI4MFRZMNQ2LYFEWO3";//MUR buyer 1 //recipient public address //TRUSTLINE HAS TO EXIST
+
+var asset_code = 'MUR2'; //arbitrarily pick one to test... This will be set by user. //can send multiple at once if wanted.
+var asset_issuer = 'GDRZOGXOBRGWN7WG5MG3LI3PFJCVWB7XCAIHUEVFHFSE3TMCKWH4YJG7';// will be checked to verify it's the real asset from MURPH
 
 // 'load' the account (really, all it's getting is the next tx sequence number)
 server.loadAccount(sender.publicKey())
@@ -32,14 +35,40 @@ server.loadAccount(sender.publicKey())
 
     // add an operation, here we're adding a single payment operation. You can add upto 100 operations in one transaction,
     // that could be 100 payments to 100 different accounts, etc...
-    builder.addOperation(
-        StellarSdk.Operation.payment({
-            destination: recipient, // destination address(random test account) //TRUSTLINE HAS TO EXIST 
-            asset: getAsset('MUR'), // see helper function above
-            amount: '1.0' // transaction amount as string
-        })
-    )
+    //send MUR
+    if(asset_code == 'MUR' && asset_issuer == "GDRZOGXOBRGWN7WG5MG3LI3PFJCVWB7XCAIHUEVFHFSE3TMCKWH4YJG7"){
+        builder.addOperation(
+            StellarSdk.Operation.payment({
+                destination: recipient, // destination address(random test account) //TRUSTLINE HAS TO EXIST 
+                asset: getAsset('MUR'), // see helper function above
+                amount: '1.0' // transaction amount as string
+            })
+        )
+    }
+    
 
+    //send MUR2
+    if(asset_code == 'MUR2' && asset_issuer == "GDRZOGXOBRGWN7WG5MG3LI3PFJCVWB7XCAIHUEVFHFSE3TMCKWH4YJG7"){
+        builder.addOperation(
+            StellarSdk.Operation.payment({
+                destination: recipient, // destination address(random test account) //TRUSTLINE HAS TO EXIST 
+                asset: getAsset('MUR2'), // see helper function above
+                amount: '5000.0' // transaction amount as string
+            })
+        )
+    }
+    
+    //send MUR3
+    if(asset_code == 'MUR3' && asset_issuer == "GDRZOGXOBRGWN7WG5MG3LI3PFJCVWB7XCAIHUEVFHFSE3TMCKWH4YJG7"){
+        builder.addOperation(
+            StellarSdk.Operation.payment({
+                destination: recipient, // destination address(random test account) //TRUSTLINE HAS TO EXIST 
+                asset: getAsset('MUR3'), // see helper function above
+                amount: '40005.99' // transaction amount as string
+            })
+        )
+    }
+    
     // create the transaction XDR
     let transaction = builder.build();
 
